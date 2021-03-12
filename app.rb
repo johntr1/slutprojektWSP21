1ac586bc6ebdde8cd5acd8088f10cb180851da5c
john-tran <55193013+johntr1@users.noreply.github.com>
2021-03-12 11:20:09 +0100

    2021-03-12

--- app.rb
+++ app.rb
@@ -9,11 +9,56 @@ get('/') do
     slim(:register)
 end
 
-get('/recipes') do
+post('/users/new') do
+    username = params[:username]
+    password = params[:password]
+    password_confirm = params[:password_confirm]
+    if password = password_confirm
+        pw_digest = BCrypt::Password.create(password)
+        db = SQLite3::Database.new('db/matreceptsida.db')
+        db.execute("INSERT INTO users (username,pw_digest) VALUES (?,?)",username,pw_digest)
+        redirect('/showlogin')
+    else
+        error_message = "Lösenorden stämmer ej! Vänligen försök igen."
+        session[:error_message] = error_message
+        session[:redirect] = "/"
+        redirect('/error')
+    end
+end
 
+get('/recipes') do
+    id = session[:id].to_i
     db = SQLite3::Database.new('db/matreceptsida.db')
     db.results_as_hash = true
-    result = db.execute("SELECT * FROM recipes WHERE")
-
-
+    result = db.execute("SELECT * FROM recipes WHERE user_id = ?", id)
+    slim(:"recipes/index", locals:{recipes:result})
 end
+
+get('/showlogin') do
+    slim(:login)
+end
+
+post('/login') do
+    username = params[:username]
+    password = params[:password]
+    db = SQLite3::Database.new('db/matreceptsida.db')
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM users WHERE username = ?",username).first
+    pwdigest = result["pw_digest"]
+    id = result["id"]
+  
+    if BCrypt::Password.new(pwdigest) == password
+      session[:id] = id
+      session[:username] = username
+      redirect('/recipes')
+    else
+      em = "Du har skrivit fel lösenord! Vänligen försök igen."
+      session[:error_message] = error_message
+      session[:redirect] = "/showlogin"
+      redirect("/error")
+    end
+end
+
+
+
+
